c5ac62d0ab15606fab2413c6627c0c8777b0f489
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2022-10-29 15:32:16 +0100

    Implemented addImage, addArticle functions and request methods

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -8,5 +8,41 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        let imageRequest = new XMLHttpRequest();
+        imageRequest.open('get', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        imageRequest.setRequestHeader('Authorization', 'Client-ID 7stfDjACsae1laOQzEhznppez0pHqSAbEsBahIAT75M');
+        imageRequest.onload = addImage;
+        imageRequest.onerror = addImageError;
+        imageRequest.send();
+
+        let articleRequest = new XMLHttpRequest();
+        articleRequest.open('get', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=eMe3prlczKkIVldFk1anWrIFLbdlydEA`);
+        articleRequest.onload = addArticle;
+        articleRequest.onerror = addArticleError;
+        articleRequest.send();
+
     });
+
+    function addImage(){
+        let data = JSON.parse(this.responseText);
+        let imageUrl = data.results[0].links.download;
+        responseContainer.insertAdjacentHTML('afterbegin', `<img src=${imageUrl} alt=${searchedForText} + " Picture" width="500px" height="300px"/>`);
+        console.log(data);
+    }
+
+    function addImageError(){
+        responseContainer.insertAdjacentHTML('beforeend', `<p>Could not find any photo by name ${searchedForText}</p>`);
+    }
+
+    function addArticle(){
+        let data = JSON.parse(this.responseText);
+        responseContainer.insertAdjacentHTML('beforeend', `<p>${data.response.docs[0].lead_paragraph}</p>`);
+        console.log(data);
+    }
+
+    function addArticleError(){
+        responseContainer.insertAdjacentHTML('beforeend', `<p>Could not find any article by name ${searchedForText}</p>`);
+    }
+
 })();
