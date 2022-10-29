32d2cf02287d5b5e10a7168d08a4d778456e5aef
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2022-10-29 16:14:07 +0100

    Refactored code

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -8,7 +8,10 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+        fetchData();
+    });
 
+    function fetchData(){
         let imageRequest = new XMLHttpRequest();
         imageRequest.open('get', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
         imageRequest.setRequestHeader('Authorization', 'Client-ID 7stfDjACsae1laOQzEhznppez0pHqSAbEsBahIAT75M');
@@ -21,14 +24,18 @@
         articleRequest.onload = addArticle;
         articleRequest.onerror = addArticleError;
         articleRequest.send();
-
-    });
+    }
 
     function addImage(){
         let data = JSON.parse(this.responseText);
-        let imageUrl = data.results[0].links.download;
-        responseContainer.insertAdjacentHTML('afterbegin', `<img src=${imageUrl} alt=${searchedForText} + " Picture" width="500px" height="300px"/>`);
-        console.log(data);
+
+        if(data && data.results && data.results[0]){
+            let imageUrl = data.results[0].links.download;
+            responseContainer.insertAdjacentHTML('afterbegin', `<img src=${imageUrl} alt=${searchedForText} + " Picture" width="500px" height="300px"/>`);
+        }
+        else{
+            responseContainer.insertAdjacentHTML('afterbegin', `<p>Could not find any photo by name ${searchedForText}</p>`);
+        }
     }
 
     function addImageError(){
@@ -37,12 +44,18 @@
 
     function addArticle(){
         let data = JSON.parse(this.responseText);
-        responseContainer.insertAdjacentHTML('beforeend', `<p>${data.response.docs[0].lead_paragraph}</p>`);
-        console.log(data);
+
+        if(data && data.response && data.response.docs.length > 1){
+            let articles = data.response.docs.map((article) => `<li><h3><a href=${article.web_url}>${article.headline.main}</a></h3><p>${article.lead_paragraph}</p></li>`)
+            responseContainer.insertAdjacentHTML('beforeend', `<ul>${articles.join('')}</ul>`);
+        }
+        else{
+            responseContainer.insertAdjacentHTML('beforeend', `<p style="color:red;">Could not find any article by name ${searchedForText}</p>`);
+        }
     }
 
     function addArticleError(){
-        responseContainer.insertAdjacentHTML('beforeend', `<p>Could not find any article by name ${searchedForText}</p>`);
+        responseContainer.insertAdjacentHTML('beforeend', `<p style="color:red;">Could not find any article by name ${searchedForText}</p>`);
     }
 
 })();
